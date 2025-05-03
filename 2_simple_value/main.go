f54057ca5fcbc58799e857d334b4731557fc017c
ZINKUNO <arpitsingh3202@gmail.com>
2025-05-03 21:13:23 +0530

    add basics of go

---
+++ 2_simple_value/main.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main(){
+	// int
+	fmt.Println(1)
+	// string
+	fmt.Println("Hello")
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+	// floats
+	fmt.Println(10.5)
+	fmt.Println(15+20)
+	fmt.Println(15.5+9.3)
+}
---
+++ 3_variables/variable.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+func main(){
+	var name string ="golang"
+	// or 
+	var name2="arpit"
+	var roll_no int =25
+	// or 
+	var roll_no2=25
+	var check =true
+	fmt.Println(roll_no2)
+	fmt.Println(name)
+	fmt.Println(roll_no)
+	fmt.Println(name2)
+	fmt.Println(check)
+	// shorthand syntx
+	name3:="arpit"
+	fmt.Println(name3)
+	var def string
+	def="arpit singh"
+	fmt.Println(def)
+	// fmt.Println()
+	// name of variable can be change 
+	name ="python"
+	fmt.Println(name)
+}
