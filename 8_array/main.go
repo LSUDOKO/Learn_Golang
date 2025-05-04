f687a6a82ab7c8161b89d04d9d786e66323d3f8a
ZINKUNO <arpitsingh3202@gmail.com>
2025-05-04 23:25:04 +0530

    learn array

---
+++ 8_array/main.go
@@ -0,0 +1,28 @@
+package main
+ 
+import "fmt"
+
+func main(){
+	// var num[4]int
+	// num[0]=1
+	// remaining element are zero
+// 	fmt.Println(num)
+// 	// lenght of array
+// 	fmt.Println(len(num))
+
+	// var vals [2]bool
+	// fmt.Println(vals)
+	// return false element 
+	// var name[3]string
+	// // name[0]="Arpit"
+	// fmt.Println(name)
+	// to declare single line array
+	// nums:=[]int{1,2,3,4,5}
+	// fmt.Println(nums)
+	// 2d array declare
+	nums:=[2][2]int {{1,2},{3,4}}
+	fmt.Println(nums)
+	// -fixed size .that is predictable 
+	// - constant time acess 
+	// - memory optimization
+}
